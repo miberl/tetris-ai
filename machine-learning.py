b0e453b90baa9eac46171a824398564fa5fc2da8
Michael Berlingieri <47005505+miberl@users.noreply.github.com>
2021-12-01 23:22:12 +0000

    Add files via upload

--- machine-learning.py
+++ machine-learning.py
@@ -3,9 +3,13 @@ from board import Board, Direction, Rotation, Action, Shape
 from exceptions import BlockLimitException
 from machinePlayer import Player, SelectedPlayer
 from adversary import RandomAdversary
-import math, time, multiprocessing, operator
-from random import seed 
+import math
+import time
+import multiprocessing
+import operator
+from random import seed
 import numpy as np
+import copy
 
 
 class Candidate:
@@ -16,7 +20,8 @@ class Candidate:
         self.Bumpiness = candidateDict['Bumpiness']
         self.Tetris = candidateDict['Tetris']
         self.RightMost = candidateDict['RightMost']
-        self.Gaps =  candidateDict['Gaps']
+        self.Gaps = candidateDict['Gaps']
+        self.after8AggrHeight = candidateDict['after8AggrHeight']
         self.after8MaxCol = candidateDict['after8MaxCol']
         self.after8CompletedLines = candidateDict['after8CompletedLines']
         self.after8Bumpiness = candidateDict['after8Bumpiness']
@@ -30,80 +35,90 @@ class Candidate:
         return value
 
     def mutate(self):
-        #Max mutation delta between -0.05 and +0.05
-        mutRate = 0.1
+        # Max mutation delta between -0.05 and +0.05
+        mutRate = 0.12
         self.Holes = self.mutateElement(self.Holes, mutRate)
         self.AggrHeight = self.mutateElement(self.AggrHeight, mutRate)
         self.CompletedLines = self.mutateElement(self.CompletedLines, mutRate)
         self.Bumpiness = self.mutateElement(self.Bumpiness, mutRate)
         self.Tetris = self.mutateElement(self.Tetris, mutRate)
         self.RightMost = self.mutateElement(self.RightMost, mutRate)
-        self.Gaps =  self.mutateElement(self.Gaps, mutRate)
+        self.Gaps = self.mutateElement(self.Gaps, mutRate)
+        self.after8AggrHeight = self.mutateElement(
+            self.after8AggrHeight, mutRate)
         self.after8MaxCol = self.mutateElement(self.after8MaxCol, mutRate)
-        self.after8CompletedLines =  self.mutateElement(self.after8CompletedLines, mutRate)
-        self.after8Bumpiness =  self.mutateElement(self.after8Bumpiness, mutRate)
-        self.after8Gaps =  self.mutateElement(self.after8Gaps, mutRate)
+        self.after8CompletedLines = self.mutateElement(
+            self.after8CompletedLines, mutRate)
+        self.after8Bumpiness = self.mutateElement(
+            self.after8Bumpiness, mutRate)
+        self.after8Gaps = self.mutateElement(self.after8Gaps, mutRate)
 
     def addFitness(self, fitnessScore):
         self.Fitness = fitnessScore
-    
+
     def clone(self):
-        return self
-        
+        return copy.deepcopy(self)
+
     def __repr__(self):
-        return 'Candidate( Holes: '+ str(self.Holes) + ' AggrHeight: '+ str(self.AggrHeight) + ' CompletedLines: '+ str(self.CompletedLines) + ' Bumpiness: '+ str(self.Bumpiness) + ' Tetris: '+ str(self.Tetris) + ' RightMost: '+ str(self.RightMost)+' Gaps: '+ str(self.Gaps)+' after8MaxCol: '+str(self.after8MaxCol)+' after8CompletedLines: '+str(self.after8CompletedLines)+' after8Bumpiness: '+str(self.after8Bumpiness)+' after8Gaps: '+str(self.after8Gaps)+' )'
+        return 'Candidate( Holes: ' + str(self.Holes) + ' AggrHeight: ' + str(self.AggrHeight) + ' CompletedLines: ' + str(self.CompletedLines) + ' Bumpiness: ' + str(self.Bumpiness) + ' Tetris: ' + str(self.Tetris) + ' RightMost: ' + str(self.RightMost)+' Gaps: ' + str(self.Gaps)+' after8AggrHeight: '+str(self.after8AggrHeight)+' after8MaxCol: '+str(self.after8MaxCol)+' after8CompletedLines: '+str(self.after8CompletedLines)+' after8Bumpiness: '+str(self.after8Bumpiness)+' after8Gaps: '+str(self.after8Gaps)+' )'
+
+
 class Population:
     def __init__(self):
         self.candidates = []
         self.rounds = 10
         self.latestTotalFitness = 0
-    
+        self.currentCandidate = None
+
     def addCandidate(self, candidate):
         self.candidates.append(candidate)
 
     def computeFitnesses(self):
         totFitness = 0
-        p = multiprocessing.Pool(8)
-        lst = list(range(len(self.candidates)))
-        results = p.map(self.computeMProc, lst)
-        for tupl in results:
-            self.candidates[tupl[0]].Fitness = tupl[1]
-            totFitness += tupl[1]
+        seeds = [8, 100, 22, 122, 612, 222, 127, 329, 431, 174]
+
+        for cand in self.candidates:
+            totScore = 0
+            self.currentCandidate = cand
+            p = multiprocessing.Pool(10)
+            results = p.map(self.computeSProc, seeds)
+            for seed, score in results:
+                totScore += score
+            cand.Fitness = totScore
+
         self.sortByFitness()
         self.latestTotalFitness = totFitness
 
-    #It is passed an index of the candidate in the list, it returns a tuple containing (index, fitnessscore)
-    def computeMProc(self, candidateIndex):
-        totScore = 0
-        seeds = [23,32,38,42,100,101,123,150,223,250]
-        for rnd in range(self.rounds):
-            board = Board(BOARD_WIDTH, BOARD_HEIGHT)
-            block_limit = 400
-            adversary = RandomAdversary(seeds[rnd], block_limit)
-            player = SelectedPlayer(self.candidates[candidateIndex])
-            try:
-                for move in board.run(player, adversary):
-                    i = board.score     
-            except:
-                pass
-            finally:
-                score = board.score
-                if board.alive is False:
-                    score = int(score*0.5)
-                totScore += score
-        return (candidateIndex, totScore)
+    def computeSProc(self, seed):
+        board = Board(BOARD_WIDTH, BOARD_HEIGHT)
+        block_limit = 400
+        adversary = RandomAdversary(seed, block_limit)
+        #player = SelectedPlayer(self.candidates[candidateIndex])
+        player = SelectedPlayer(self.currentCandidate)
+        try:
+            for move in board.run(player, adversary):
+                i = board.score
+        except:
+            pass
+        finally:
+            score = board.score
+            print(seed, score)
+        return (seed, score)
 
     def sortByFitness(self):
-        self.candidates = sorted(self.candidates, key=operator.attrgetter("Fitness"), reverse=True)
+        self.candidates = sorted(
+            self.candidates, key=operator.attrgetter("Fitness"), reverse=True)
 
     def naturalSelection(self):
         newCandidates = []
         self.sortByFitness()
         parent = self.candidates[0]
         child = parent.clone()
-        
         newCandidates.append(child)
-        mutatedchild = child.clone().mutate()
+
+        mutatedchild = child.clone()
+        mutatedchild.mutate()
+
         newCandidates.append(mutatedchild)
 
         while len(newCandidates) < len(self.candidates):
@@ -114,52 +129,52 @@ class Population:
 
         self.candidates = newCandidates
 
-
     def selectCandidate(self):
         rand = np.random.random(1)[0]*self.latestTotalFitness
         sum = 0
         for cand in self.candidates:
-            sum+= cand.Fitness 
+            sum += cand.Fitness
             if sum > rand:
                 return cand
         return None
 
-
     def getLength(self):
         return len(self.candidates)
 
+# Variance between 0.95 and 1.05
+
+
 def percentageDelta(value):
-    variance = value*0.05
+    variance = value*0.2
+    #variance = value*0.05
     return value + np.random.random(1)[0]*2*variance - variance
-    
 
 
-#this could be an initial element of population
+# this could be an initial element of population
 def generateRandomCandidate():
-    # Generating random values between -0.5 and 0.5 for each characteristic 
+    # Generating random values between -0.5 and 0.5 for each characteristic
 
-    #Not truly random, starting from known heuristics
+    # Not truly random, starting from known heuristics
 
-    randCandidate = {   'Holes':  -4.9,
-                    'AggrHeight': -0.001, 
-                    'CompletedLines' :-0.4,
-                    'Bumpiness' : -0.01,
-                    'Tetris' : 1000,
-                    'RightMost' : -0.4,
-                    'Gaps': -0.2,
-                    'after8MaxCol':-1,
-                    'after8CompletedLines': 0.01,
-                    'after8Bumpiness':-0.15,
-                    'after8Gaps': -0.1
-                    }
+    randCandidate = {'Holes': -10.425848148861757,
+                     'AggrHeight': -0.0009293794396322049,
+                     'CompletedLines': -2.5023832192570596,
+                     'Bumpiness': -0.04355179063623546,
+                     'Tetris': 102.45373845404774,
+                     'RightMost': -0.45308411439364954,
+                     'Gaps': -0.527280443244512,
+                     'after8AggrHeight': -0.008003913016549473,
+                     'after8MaxCol': -1.2818902163686863,
+                     'after8CompletedLines': 0.0012453808091311908,
+                     'after8Bumpiness': -0.1088077254081058,
+                     'after8Gaps': -0.20168757364913825
+                     }
 
     return randCandidate
 
-    
-
 
 def startLearning():
-    popSize = 16
+    popSize = 10
     pop = Population()
     c = Candidate(generateRandomCandidate())
     pop.addCandidate(c)
@@ -167,44 +182,36 @@ def startLearning():
         c = Candidate(generateRandomCandidate())
         c.mutate()
         pop.addCandidate(c)
-    
-    
-    
+
     generation = 0
-    
+
     print('Calculating fitness for gen=0 (random) population')
     pop.computeFitnesses()
     pop.sortByFitness()
 
-    #best performing candidate
+    # best performing candidate
 
-    
     print('Average Fitness: ', pop.latestTotalFitness//pop.getLength())
     print('Highest Fitness: ', pop.candidates[0].Fitness)
     print('Fittest candidate values: ', pop.candidates[0])
-    print('Highest AVG round Fitness:',pop.candidates[0].Fitness//pop.rounds )
-    
+    print('Highest AVG round Fitness:', pop.candidates[0].Fitness//pop.rounds)
 
-    
-    
-    
     while True:
 
         pop.naturalSelection()
 
-        generation+=1
-        print('Checking fitness of new candidates, gen=',generation)
+        generation += 1
+        print('Checking fitness of new candidates, gen=', generation)
 
         pop.computeFitnesses()
         pop.sortByFitness()
-        
-    
+
         print('Average Fitness: ', pop.latestTotalFitness//pop.getLength())
         print('Highest Fitness: ', pop.candidates[0].Fitness)
         print('Fittest candidate values: ', pop.candidates[0])
-        print('Highest AVG round Fitness:',pop.candidates[0].Fitness//pop.rounds)
+        print('Highest AVG round Fitness:',
+              pop.candidates[0].Fitness//pop.rounds)
 
-        
-    
+
 if __name__ == '__main__':
     startLearning()
--- machinePlayer.py
+++ machinePlayer.py
@@ -7,14 +7,19 @@ import time
 holeCoeff = 0
 heightCoeff = 0
 onetothreeCoeff = 0
-bumpsCoeff= 0
+bumpsCoeff = 0
 tetrisCoeff = 0
 rightMostCoeff = 0
 gapsCoeff = 0
 after8MaxColCoeff = 0
+after8AggrHeight = 0
 after8OnetothreeCoeff = 0
 after8BumpsCoeff = 0
-after8GapsCoeff = 0 
+after8GapsCoeff = 0
+
+lastScore = 0
+lastHoles = 0
+discards = 0
 
 
 class Player:
@@ -22,10 +27,9 @@ class Player:
         raise NotImplementedError
 
 
-
 class RandomPlayer(Player):
     def __init__(self, seed=None):
-        self.random = Random(seed)
+        self.random = Random(1)
 
     def choose_action(self, board):
         if self.random.random() > 0.97:
@@ -43,49 +47,50 @@ class RandomPlayer(Player):
                 Rotation.Anticlockwise,
                 Rotation.Clockwise,
             ])
-#Board.falling -> contains information about the currently falling block
-#its shape (board.falling.shape) and the coordinates of its cells (board.falling.cells). 
+# Board.falling -> contains information about the currently falling block
+# its shape (board.falling.shape) and the coordinates of its cells (board.falling.cells).
 # The shape is an instance of the enum shape, whose definition can be found in board.py.
 
 
-#Heuristics
-#Bumpiness
-#Holes
-#Completed lines 
-#Aggregate height
+# Heuristics
+# Bumpiness
+# Holes
+# Completed lines
+# Aggregate height
 
-#this should return the final board position, with the list of moves it took to accomplish it 
+# this should return the final board position, with the list of moves it took to accomplish it
 
 
-
-#Returns a score for a board
+# Returns a score for a board
 def hisHolyness(sandbox):
     holes = 0
-    for y in range (sandbox.height):
-        for x in range (sandbox.width):
-            if (x,y) not in sandbox.cells:
-                for upperYs in range(0,y):
+    for y in range(sandbox.height):
+        for x in range(sandbox.width):
+            if (x, y) not in sandbox.cells:
+                for upperYs in range(0, y):
                     if (x, upperYs) in sandbox.cells:
-                        holes+=1
+                        holes += 1
                         break
     return holes
 
+
 def hisHighNess(sandbox):
     highness = 0
-    for x in range (sandbox.width):
-        highness+=columnHeight(sandbox, x)
-    return highness
+    for x in range(sandbox.width):
+        highness += columnHeight(sandbox, x)
+    return highness ** 2
+
 
 def columnHeight(sandbox, x):
     height = 0
-    for y in range (0, sandbox.height):
-        if (x,y) in sandbox.cells:
+    for y in range(0, sandbox.height):
+        if (x, y) in sandbox.cells:
             height += (sandbox.height-y)
             break
     return height
 
 
-#Maybe allow one bump of 4 blocks 
+# Maybe allow one bump of 4 blocks
 def hisBumpiness(sandbox):
     bumpy = 0
     diff = 0
@@ -97,46 +102,65 @@ def hisBumpiness(sandbox):
     return bumpy
 
 
-
 def Tetris(sandbox, complines):
-    if complines >= 4:
-        return 1 
-    return 0
+    global lastScore
 
-    
+    diffScore = sandbox.score - lastScore
+    # We have a tetris
+    if complines >= 4:
+        return 1
+    else:
+        return 0
+
+    '''
+    if complines >= 1:
+        # if complines >= 4:
+        assert(False)
+        return 1
+    return 0
+    '''
 
 
 def realCompletedLines(sandbox):
-    linesCompleted = 0
-    for y in range (sandbox.height):
-        allComplete = True
-        for x in range (sandbox.width):
-            if (x,y) not in sandbox.cells:
-                allComplete = False
-                break
-        if allComplete:
-            linesCompleted +=1
-    return linesCompleted
 
-##Between 3 a
+    global lastScore
+    diffScore = sandbox.score - lastScore
+
+    if diffScore >= 1600:
+        return 4
+    elif diffScore >= 400:
+        return 3
+    elif diffScore >= 100:
+        return 2
+    elif diffScore >= 42:
+        return 1
+    else:
+        return 0
+
+# Between 3 a
+
+
 def completedLines(sandbox, complines):
     if complines < 4:
         return complines
-    return 0 
+    return 0
 
-#Rightmost line should be free 
+# Rightmost line should be free
+
+
 def freeRightMostLine(sandbox):
     ch = columnHeight(sandbox, 9)
     if ch != 0:
         return 1
-    else: 
-        return 0
+    return 0
+
 
 def bigContinuousBlock(sandbox, holes, height):
     hol = holes
-    if hol == 0 and height< 110:
+    if hol == 0 and height < 110:
         return 1
     return 0
+
 
 def maxLineHeight(sandbox):
     maxHeight = 0
@@ -146,6 +170,7 @@ def maxLineHeight(sandbox):
             maxHeight = hx
     return maxHeight
 
+
 def bigGaps(sandbox):
     gaps = 0
     for column in range(1, sandbox.width):
@@ -154,15 +179,11 @@ def bigGaps(sandbox):
         deltaH = chCurrentColumn - chLeft
         if deltaH > 0:
             gaps += deltaH
-    return gaps
+    return gaps ** 2
 
 
 def evalBoard(sandbox):
-    #score = -0.35663*hisHolyness(sandbox) - 0.510066* hisHighNess(sandbox) +0.760666 * completedLines(sandbox) -0.184483*hisBumpiness(sandbox)
-    #Penalise moves that cover up a hole on the edges without doing a tetris
-    #score = -0.35663*hisHolyness(sandbox) - 0.510066* hisHighNess(sandbox) +0.760666 * ((completedLines(sandbox)-2)**completedLines(sandbox)) -0.184483*hisBumpiness(sandbox)
-    #These heuristics have been taken from https://codemyroad.wordpress.com/2013/04/14/tetris-ai-the-near-perfect-player/
-    #score = -0.35663*hisHolyness(sandbox) - 0.510066* hisHighNess(sandbox) +0.760666 * ((completedLines(sandbox)-2)**completedLines(sandbox)) -0.184483*hisBumpiness(sandbox)
+    global holeCoeff, heightCoeff, onetothreeCoeff, bumpsCoeff, tetrisCoeff, rightMostCoeff, gapsCoeff, after8MaxColCoeff, after8OnetothreeCoeff, after8BumpsCoeff, after8GapsCoeff, after8AggrHeight
     holes = hisHolyness(sandbox)
     height = hisHighNess(sandbox)
     completedline = realCompletedLines(sandbox)
@@ -170,23 +191,44 @@ def evalBoard(sandbox):
     bumps = hisBumpiness(sandbox)
     tetris = Tetris(sandbox, completedline)
     righMostLine = freeRightMostLine(sandbox)
-    bigBlock = bigContinuousBlock(sandbox, holes, height)
     maximumColumnHeight = maxLineHeight(sandbox)
     gaps = bigGaps(sandbox)
 
+    # Old coefficients Coefficients
+    if maximumColumnHeight > 13 or holes > 0:
+        return holeCoeff*holes + after8OnetothreeCoeff * onetothreelines + after8BumpsCoeff*bumps + tetrisCoeff*tetris + after8AggrHeight*height+rightMostCoeff * righMostLine + after8MaxColCoeff*maximumColumnHeight + after8GapsCoeff*gaps
 
+    score = +holeCoeff*holes + heightCoeff * height + onetothreeCoeff * onetothreelines + \
+        bumpsCoeff*bumps + tetrisCoeff*tetris + \
+        rightMostCoeff * righMostLine + gapsCoeff*gaps
 
-    #Old coefficients Coefficients 
-
-    if maximumColumnHeight > 8 or holes >0:
-        return holeCoeff*holes +heightCoeff* height +after8OnetothreeCoeff* onetothreelines +after8BumpsCoeff*bumps +tetrisCoeff*tetris +rightMostCoeff* righMostLine + after8MaxColCoeff*maximumColumnHeight + after8GapsCoeff*gaps 
-        #return -0.35663*hisHolyness(sandbox) - 0.510066* hisHighNess(sandbox) +tetris*100+0.760666 * realCompletedLines(sandbox) -0.184483*hisBumpiness(sandbox)
-
-
-    score = +holeCoeff*holes +heightCoeff* height +onetothreeCoeff* onetothreelines +bumpsCoeff*bumps +tetrisCoeff*tetris +rightMostCoeff* righMostLine + gapsCoeff*gaps 
-
-    
     return score
+    '''
+    holeCoeff = -9.142298868973754
+    heightCoeff = -0.0009361130217256599
+    onetothreeCoeff = -2.50238321925705955
+    # Changed from -0.05
+    bumpsCoeff = -0.05
+    tetrisCoeff = 102.45373845404774
+    rightMostCoeff = -0.40264836811111115
+    gapsCoeff = -0.527280443244512
+    maxColCoeff = 0
+
+   # Old coefficients Coefficients
+
+    if maximumColumnHeight > 13 or holes > 0:
+        maxColCoeff = -1.046404648857806
+        heightCoeff = -0.009755359766674137
+        onetothreeCoeff = 0.0009443446375252447
+        bumpsCoeff = -0.13783132835025366
+        gapsCoeff = -0.20168757364913825
+
+    score = +holeCoeff*holes + heightCoeff * height + onetothreeCoeff * onetothreelines + bumpsCoeff*bumps + tetrisCoeff * \
+        tetris + rightMostCoeff * righMostLine + maxColCoeff * \
+        maximumColumnHeight + gapsCoeff*gaps  # + 0.99* bigBlock
+
+    return score
+    '''
 
 
 def moveHorizontally(sandbox, previousMoves, direction, shifts):
@@ -198,105 +240,102 @@ def moveHorizontally(sandbox, previousMoves, direction, shifts):
         moves.append(direction)
         if ret:
             break
-    
+
     if not ret:
         newSandbox.move(Direction.Drop)
         moves.append(Direction.Drop)
-    
+
     return newSandbox, moves
 
 
 def findHorizontalMoves(sandbox, previousMoves):
     bestAction = []
     bestScore = -1000000
-
+    bestSandbox = None
     if sandbox.falling is not None:
         leftmoves = distanceFrom0X(sandbox)
         rightmoves = 10-findPieceWidth(sandbox) - leftmoves
-        
-        
 
         for move in range(0, leftmoves):
-            movedSandbox, moves = moveHorizontally(sandbox, previousMoves, Direction.Left, move)
+            movedSandbox, moves = moveHorizontally(
+                sandbox, previousMoves, Direction.Left, move)
             newSandboxScore = 0
-            
-            if sandbox.next is not None:
-                newBoard = movedSandbox.clone()
-                newSandboxScore += chooseBestMove(newBoard)[1]
-    
-            newSandboxScore += evalBoard(movedSandbox)
-            if (newSandboxScore > bestScore):
-                bestAction = moves.copy()
-                bestScore = newSandboxScore
-                
-                
 
-        for move in range(0,rightmoves):
-            movedSandbox, moves = moveHorizontally(sandbox, previousMoves, Direction.Right, move)
-            newSandboxScore = 0
-            
             if sandbox.next is not None:
                 newBoard = movedSandbox.clone()
                 newSandboxScore += chooseBestMove(newBoard)[1]
 
             newSandboxScore += evalBoard(movedSandbox)
+            if (newSandboxScore > bestScore):
+                bestAction = moves.copy()
+                bestScore = newSandboxScore
+                bestSandbox = movedSandbox
+
+        for move in range(0, rightmoves):
+            movedSandbox, moves = moveHorizontally(
+                sandbox, previousMoves, Direction.Right, move)
+            newSandboxScore = 0
+
+            if sandbox.next is not None:
+                newBoard = movedSandbox.clone()
+                newSandboxScore += chooseBestMove(newBoard)[1]
+
+            newSandboxScore += evalBoard(movedSandbox)
 
             if (newSandboxScore > bestScore):
                 bestAction = moves.copy()
                 bestScore = newSandboxScore
-
-        ##Neither right or lef, just drop centrally
+                bestSandbox = movedSandbox
+        # Neither right or lef, just drop centrally
         moves = previousMoves.copy()
         movedSandbox = sandbox.clone()
         newSandboxScore = 0
         if movedSandbox.falling:
             movedSandbox.move(Direction.Drop)
             moves.append(Direction.Drop)
-        
+
         if sandbox.next is not None:
             newBoard = movedSandbox.clone()
             newSandboxScore += chooseBestMove(newBoard)[1]
         newSandboxScore += evalBoard(movedSandbox)
-        
+
         if (newSandboxScore > bestScore):
             bestAction = moves.copy()
             bestScore = newSandboxScore
+            bestSandbox = movedSandbox
+    return bestScore, bestAction, bestSandbox
 
-    return bestScore, bestAction
 
 def distanceFrom0X(sandbox):
-    min =100 
+    min = 100
     for pair in sandbox.falling.cells:
-        if (pair[0]<min):
+        if (pair[0] < min):
             min = pair[0]
     return min
+
 
 def findPieceWidth(sandbox):
     min = 100
     max = -1
     for pair in sandbox.falling.cells:
-        if (pair[0]>max):
+        if (pair[0] > max):
             max = pair[0]
-        if (pair[0]<min):
+        if (pair[0] < min):
             min = pair[0]
     return (max-min+1)
 
-def someDirection(list, move):
-    l = list.copy()
-    l.append(move)
-    return l
 
 def chooseBestMove(sandbox_original):
     bestScore = -1000000
     bestAction = []
-    
+    bestSandbox = None
     if sandbox_original.falling is not None:
         rots = 4
         if sandbox_original.falling.shape == Shape.O:
             rots = 1
         if sandbox_original.falling.shape == Shape.I:
-            rots=2
-        for rotation in range(0,rots):
+            rots = 2
+        for rotation in range(0, rots):
             sandbox = sandbox_original.clone()
             if sandbox.falling is not None:
                 actions = []
@@ -320,36 +359,49 @@ def chooseBestMove(sandbox_original):
                     except:
                         pass
                     actions.append(Rotation.Anticlockwise)
-            
-            score, totalActions = findHorizontalMoves(sandbox, actions)
+
+            score, totalActions, sandbox = findHorizontalMoves(
+                sandbox, actions)
             if score > bestScore:
                 bestAction = totalActions.copy()
                 bestScore = score
-    return bestAction, bestScore
+                bestSandbox = sandbox
+    return bestAction, bestScore, bestSandbox
 
 
 class MichaelsPlayer(Player):
     def __init__(self, candidate, seed=None):
-        #Could initialise parameters from here?
+        # Could initialise parameters from here?
         self.random = Random(seed)
-        global holeCoeff, heightCoeff, onetothreeCoeff, bumpsCoeff, tetrisCoeff, rightMostCoeff, gapsCoeff, after8MaxColCoeff, after8OnetothreeCoeff, after8BumpsCoeff, after8GapsCoeff
+
+        global holeCoeff, heightCoeff, onetothreeCoeff, bumpsCoeff, tetrisCoeff, rightMostCoeff, gapsCoeff, after8MaxColCoeff, after8OnetothreeCoeff, after8BumpsCoeff, after8GapsCoeff, after8AggrHeight
         holeCoeff = candidate.Holes
         heightCoeff = candidate.AggrHeight
         onetothreeCoeff = candidate.CompletedLines
-        bumpsCoeff= candidate.Bumpiness
+        bumpsCoeff = candidate.Bumpiness
         tetrisCoeff = candidate.Tetris
         rightMostCoeff = candidate.RightMost
         gapsCoeff = candidate.Gaps
+        after8AggrHeight = candidate.after8AggrHeight
         after8MaxColCoeff = candidate.after8MaxCol
         after8OnetothreeCoeff = candidate.after8CompletedLines
         after8BumpsCoeff = candidate.after8Bumpiness
         after8GapsCoeff = candidate.after8Gaps
 
     def choose_action(self, board):
-        bestMove = chooseBestMove(board)[0]
-        return bestMove
-        
+        global lastHoles, lastScore, discards
+        lastHoles = hisHolyness(board)
+        lastScore = board.score
 
-    
-        
+        bestMove = chooseBestMove(board)
+
+        newHoles = hisHolyness(bestMove[2])
+
+        if (newHoles > 0 and discards < 10):
+            discards += 1
+            return Action.Discard
+
+        return bestMove[0]
+
+
 SelectedPlayer = MichaelsPlayer
